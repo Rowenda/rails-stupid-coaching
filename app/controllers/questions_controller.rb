880916bdf6b73234e8c5f320186d9722b6f8b091
Rowenda <badbat_69@hotmail.com>
2023-11-26 15:14:53 +0100

    improve stupid-coaching for practising

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,12 +5,22 @@ class QuestionsController < ApplicationController
 
   def answer
     @answer = params['question']
-    @coach_message = if @answer.capitalize == 'I am going to work'
-                       'Great!'
-                     elsif @answer.last == '?'
-                       'Silly question, get dressed and go to work!'
-                     else
-                       "I don't care, get dressed and go to work!"
-                     end
+    if @answer == @answer.upcase
+      @coach_message = "I can feel your motivation! " + coach_response(@answer)
+    else
+      @coach_message = coach_response(@answer)
+    end
+  end
+
+  private
+  def coach_response(answer)
+    if answer.capitalize == 'I am going to work'
+      @coach_message = 'Great!'
+    elsif answer.last == '?'
+      @coach_message = 'Silly question, get dressed and go to work!'
+    else
+      @coach_message = "I don't care, get dressed and go to work!"
+    end
+  return @coach_message
   end
 end
